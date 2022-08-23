519e395cc4adf94c01b566dcf0d90fd8e8468818
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-08-23 15:49:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+
+const app = express(); // Initialize express as an app variable
+app.set("port", process.env.PORT || 3050); // Set the port
+app.use(express.json()); // Enable the server to handle JSON requests
+app.use(cors()); // Dont let local development give errors
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+const userRoute = require("./routes/userRoute");
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+app.get("/users", userRoute);
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening for calls on port ${app.get("port")}`);
+  console.log("Press Ctrl+C to exit server");
+});
+
+module.exports = {
+  devServer: {
+    Proxy: "*",
+  },
+};
